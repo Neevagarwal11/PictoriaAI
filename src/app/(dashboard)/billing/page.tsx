0a59432f4f937eae658e85b97d19d32f1ab40edb
Neev Agarwal <agarwalneev2005@gmail.com>
2025-07-11 22:15:36 +0530

    Stripe Integration Started

--- src/app/(dashboard)/billing/page.tsx
+++ src/app/(dashboard)/billing/page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Billingpage() {
   return (
-    <div>page</div>
+    <section className='container mx-auto space-y-8'>
+      <div>
+        <h1 className='text-3xl font-bold tracking-tight'>Plans and Billing</h1>
+        <p className='text-muted-foreground'> 
+          Manage your subscription and billing information.
+        </p>
+      </div>
+
+    </section>
   )
 }
 
